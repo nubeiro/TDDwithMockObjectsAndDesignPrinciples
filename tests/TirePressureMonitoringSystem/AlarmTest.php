67baf780f37758ef97033ed5c9cf2df6e4bc52af
Raul Araya Tauler <nubeiro@gmail.com>
2015-04-17 17:10:31 +0200

    Save second session changes.

--- tests/TirePressureMonitoringSystem/AlarmTest.php
+++ tests/TirePressureMonitoringSystem/AlarmTest.php
@@ -2,30 +2,85 @@
 
 use \TDDMicroExercises\PHP\TirePressureMonitoringSystem\Alarm;
 
-class AlarmTest extends PHPUnit_Framework_TestCase {
+class AlarmTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Alarm
+     */
+    protected $objectToTest;
 
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $sensorMock;
 
-    public function testInstantiation()
+    public function setUp()
     {
-//        $obj = new Alarm();
-    }
-
-    public function testCheck()
-    {
-        $tested_value = 15;
-        $mock_sensor = $this->getMockBuilder('\TDDMicroExercises\PHP\TirePressureMonitoringSystem\Sensor')
+        $this->sensorMock = $this->getMockBuilder('\TDDMicroExercises\PHP\TirePressureMonitoringSystem\Sensor')
             ->setMethods(array('popNextPressurePsiValue'))
             ->getMock();
-        $mock_sensor
+        $this->objectToTest = new Alarm($this->sensorMock);
+    }
+
+    public function testConstructor()
+    {
+        $this->assertAttributeEquals(0, 'alarmCount', $this->objectToTest, "WRONG");
+        $this->assertAttributeEquals(false, 'alarmOn', $this->objectToTest, "WRONG ON");
+    }
+
+    public function checkDataProvider()
+    {
+        return array(
+            "low pressure alarm" => array (
+                "pressure" => Alarm::LOW_PRESSURE_TRESHOLD - 1,
+                "expected alarm" => true,
+                "expected internal counter" => 1
+            ),
+            "low pressure border value" => array (
+                "pressure" => Alarm::LOW_PRESSURE_TRESHOLD,
+                "expected alarm" => false,
+                "expected internal counter" => 0
+            ),
+            "value in the middle" => array (
+                "pressure" => rand(Alarm::LOW_PRESSURE_TRESHOLD, Alarm::HIGH_PRESSURE_TRESHOLD),
+                "expected alarm" => false,
+                "expected internal counter" => 0
+            ),
+            "high pressure alarm" => array (
+                "pressure" => Alarm::HIGH_PRESSURE_TRESHOLD + 1,
+                "expected alarm" => true,
+                "expected internal counter" => 1
+            ),
+            "high pressure border value" => array (
+                "pressure" => Alarm::HIGH_PRESSURE_TRESHOLD,
+                "expected alarm" => false,
+                "expected internal counter" => 0
+            )
+        );
+    }
+
+    /**
+     * @dataProvider checkDataProvider
+     */
+    public function testCheck($tested_value, $expectedAlarmValue, $expectedInternalCounter)
+    {
+        $this->sensorMock
             ->expects($this->once())
             ->method('popNextPressurePsiValue')
             ->will($this->returnValue($tested_value));
-        $tested_obj = new Alarm($mock_sensor);
-        $this->assertAttributeEquals(0, 'alarmCount', $tested_obj, "WRONG");
-        $this->assertAttributeEquals(false, 'alarmOn', $tested_obj, "WRONG ON");
-        $tested_obj->check();
-        $this->assertAttributeEquals(1, 'alarmCount', $tested_obj, "WRONG2");
-        $this->assertAttributeEquals(true, 'alarmOn', $tested_obj, "WRONG ON2");
 
+        $this->objectToTest->check();
+        $this->assertAttributeEquals(
+            $expectedInternalCounter,
+            'alarmCount',
+            $this->objectToTest,
+            "Wrong internal counter value"
+        );
+        $this->assertAttributeEquals(
+            $expectedAlarmValue,
+            'alarmOn',
+            $this->objectToTest,
+            "Wrong internal alarm value"
+        );
     }
 }
